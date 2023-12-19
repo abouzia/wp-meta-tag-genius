90b80da5cc2f71e96628ccaf27cc1111aad23411
Yassine <yassine.abouzia@gmail.com>
2023-12-19 15:07:05 +0100

    get and update options to database

--- meta-tags-genius.php
+++ meta-tags-genius.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Meta Tags Genius
 Description: A powerful WordPress plugin for managing meta tags with ease.
-Version: 0.0.1
+Version: 0.0.1-beta
 Author: Yassine Abouzia
 License: GPL v2 or later
 */
@@ -22,9 +22,26 @@ class MetaTagsGenius
         add_action('admin_menu', array($this, 'mtg_admin_menu'));
     }
 
+    
+
     public function activate()
     {
-        get_option('mtg_meta_tags_options') || add_option('mtg_meta_tags_options', []);
+        $this->mtg_get_options();
+    }
+    
+    function mtg_get_options()
+    {
+        $mtg_options = get_option('mtg_meta_tags_options', []);
+        
+        if (empty($mtg_options)) {
+            $defaults = [
+                'author' => 'dummy author tag.',
+                'content' => 'dummy content tag.'
+            ];
+            update_option('mtg_meta_tags_options', $defaults);
+        }
+
+        return $mtg_options;
     }
 
     function mgt_admin_enqueue_scripts()
@@ -34,7 +51,37 @@ class MetaTagsGenius
 
     function mtg_admin_init()
     {
-        add_action('admin_post_mtg_save_action', array($this, 'mtg_save_action'));
+        add_action('admin_post_mtg_save_action', array($this, 'mtg_admin_save'));
+    }
+
+    function mtg_admin_save()
+    {
+        // Check if user can access this page
+        if (!current_user_can('manage_options')) {
+            wp_die(__('You do not have sufficient permissions to access this page.'));
+        }
+
+        // Check if nonce is valid
+        check_admin_referer('mtg_save_action', 'mtg_nonce');
+        
+        // check if all post values not empty
+        if (empty($_POST['mtg_author_input']) || empty($_POST['mtg_content_input'])) {
+            wp_die(__('Please fill all the fields.'));
+        }
+
+        // convert post values to variables
+        extract($_POST);
+
+        // sanitize post values and save them to database
+        $updated_options = [
+            'author' => sanitize_text_field($mtg_author_input),
+            'content' => sanitize_text_field($mtg_content_input)
+        ];
+        update_option('mtg_meta_tags_options', $updated_options);
+
+        // redirect to options page
+        wp_redirect(admin_url('admin.php?page=mtg'));
+        exit;
     }
 
     function mtg_admin_menu()
@@ -50,25 +97,37 @@ class MetaTagsGenius
         );
     }
 
-    function mtg_options_page()
+    function mtg_meta_tags_options_html($meta_tags_options)
     {
+        // meta tags array to variables
+        extract($meta_tags_options);
 
-        if (!current_user_can('manage_options')) {
-            wp_die(__('You do not have sufficient permissions to access this page.'));
-        }
-
+        // html
         $html = '<div class="mtg-container">';
         $html .= '<h1 class="mtg-title">Meta Tags Genius</h1>';
-        $html .= "<h2>Welcome</h2>";
+
+        // form
         $html .= '<form method="post" action="admin-post.php">';
         $html .= '<input type="hidden" name="action" value="mtg_save_action">';
         $html .= wp_nonce_field('mtg_save_action', 'mtg_nonce');
+
+        // author form group
         $html .= '<div class="mtg-form-group">';
         $html .= '<label for="mtg_author_input">';
-        $html .= 'Author: ';
+        $html .= 'Meta Author: ';
         $html .= '</label>';
-        $html .=  "<input type='text' id='mtg_author_input' name='mtg_author_input''>";
+        $html .=  "<input type='text' id='mtg_author_input' name='mtg_author_input' value='$author'>";
         $html .= '</div>';
+
+        // content form group
+        $html .= '<div class="mtg-form-group">';
+        $html .= '<label for="mtg_content_input">';
+        $html .= 'Meta Conent: ';
+        $html .= '</label>';
+        $html .=  "<input type='text' id='mtg_content_input' name='mtg_content_input' value='$content'>";
+        $html .= '</div>';
+
+        // save button
         $html .=  '<input type="submit" id="mtg_save_button" name="mtg_save_button" value="Save">';
         $html .= '</form>';
         $html .= '</div>';
@@ -76,6 +135,22 @@ class MetaTagsGenius
 
         echo $html;
     }
+
+    function mtg_options_page() {
+
+        // check if user can access this page
+        if (!current_user_can('manage_options')) {
+            wp_die(__('You do not have sufficient permissions to access this page.'));
+        }
+        
+        // get meta tags options
+        $meta_tags_options = get_option('mtg_meta_tags_options');
+
+        // show meta tags options page
+        $this->mtg_meta_tags_options_html($meta_tags_options);
+    }
+    
 }
+
 
 new MetaTagsGenius();
