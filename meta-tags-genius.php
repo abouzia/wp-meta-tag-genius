b21072f8c7fa96c6649a19ad42ed7510136f9eda
Yassine <yassine.abouzia@gmail.com>
2023-12-20 15:08:17 +0100

    add keywords tag and improve performance

--- meta-tags-genius.php
+++ meta-tags-genius.php
@@ -22,29 +22,30 @@ class MetaTagsGenius
         add_action('admin_menu', array($this, 'mtg_admin_menu'));
 
         // Add meta tags to the head
-        add_action('wp_head', array($this, 'mtg_meta_tags_to_head'), 99);
+        add_action('wp_head', array($this, 'mtg_meta_tags_to_head'), 1);
     }
 
 
 
     public function activate()
     {
-        $this->mtg_get_options();
+        $this->mtg_get_meta_tags();
     }
 
-    function mtg_get_options()
+    function mtg_get_meta_tags()
     {
-        $mtg_options = get_option('mtg_meta_tags_options', []);
+        $mtg_meta_tags = get_option('mtg_meta_tags_options', []);
 
         if (empty($mtg_options)) {
             $defaults = [
                 'author' => 'dummy author tag.',
-                'content' => 'dummy content tag.'
+                'description' => 'dummy content tag.',
+                'keywords' => 'dummy keywords tag.',
             ];
             update_option('mtg_meta_tags_options', $defaults);
         }
 
-        return $mtg_options;
+        return $mtg_meta_tags;
     }
 
     function mgt_admin_enqueue_scripts()
@@ -54,10 +55,15 @@ class MetaTagsGenius
 
     function mtg_meta_tags_to_head()
     {
-        $mtg_options = $this->mtg_get_options();
-        extract($mtg_options);
-        echo "<meta name='author' content='$author'>";
-        echo "<meta name='description' content='$content'>";
+        $meta_tags = $this->mtg_get_meta_tags();
+        extract($meta_tags);
+
+        foreach ($meta_tags as $key => $value) {
+            if (empty($value)) {
+                continue;
+            }
+            echo "<meta name='$key' content='$value'>\n";
+        }
     }
 
     function mtg_admin_init()
@@ -76,7 +82,11 @@ class MetaTagsGenius
         check_admin_referer('mtg_save_action', 'mtg_nonce');
 
         // check if all post values not empty
-        if (empty($_POST['mtg_author_input']) || empty($_POST['mtg_content_input'])) {
+        if (
+            empty($_POST['mtg_author_input']) ||
+            empty($_POST['mtg_description_input']) ||
+            empty($_POST['mtg_keywords_input'])
+        ) {
             wp_die(__('Please fill all the fields.'));
         }
 
@@ -86,7 +96,8 @@ class MetaTagsGenius
         // sanitize post values and save them to database
         $updated_options = [
             'author' => sanitize_text_field($mtg_author_input),
-            'content' => sanitize_text_field($mtg_content_input)
+            'description' => sanitize_text_field($mtg_description_input),
+            'keywords' => sanitize_text_field($mtg_keywords_input),
         ];
         $is_updated = update_option('mtg_meta_tags_options', $updated_options);
 
@@ -116,10 +127,10 @@ class MetaTagsGenius
         );
     }
 
-    function mtg_meta_tags_options_html($meta_tags_options)
+    function mtg_meta_tags_options_html($meta_tags)
     {
         // meta tags array to variables
-        extract($meta_tags_options);
+        extract($meta_tags);
 
         // html
         $html = '<div class="mtg-container">';
@@ -130,6 +141,17 @@ class MetaTagsGenius
         $html .= '<input type="hidden" name="action" value="mtg_save_action">';
         $html .= wp_nonce_field('mtg_save_action', 'mtg_nonce');
 
+        // form groups
+        foreach ($meta_tags as $key => $value) {
+            $html .= '<div class="mtg-form-group">';
+            $html .= '<label for="mtg_' . $key . '_input">';
+            $html .= ucfirst($key) . ': ';
+            $html .= '</label>';
+            $html .=  "<input type='text' id='mtg_" . $key . "_input' name='mtg_" . $key . "_input' value='$value'>";
+            $html .= '</div>';
+        }
+
+        /*
         // author form group
         $html .= '<div class="mtg-form-group">';
         $html .= '<label for="mtg_author_input">';
@@ -138,15 +160,27 @@ class MetaTagsGenius
         $html .=  "<input type='text' id='mtg_author_input' name='mtg_author_input' value='$author'>";
         $html .= '</div>';
 
-        // content form group
+        
+
+        // description form group
         $html .= '<div class="mtg-form-group">';
-        $html .= '<label for="mtg_content_input">';
-        $html .= 'Meta Conent: ';
+        $html .= '<label for="mtg_description_input">';
+        $html .= 'Meta Description: ';
         $html .= '</label>';
-        $html .= "<textarea id='mtg_content_input' name='mtg_content_input' rows='4' cols='50'>";
-        $html .= $content;
+        $html .= "<textarea id='mtg_description_input' name='mtg_description_input' rows='4' cols='50'>";
+        $html .= $description;
         $html .= '</textarea>';
         $html .= '</div>';
+
+        // keywords form group
+        $html .= '<div class="mtg-form-group">';
+        $html .= '<label for="mtg_keywords_input">';
+        $html .= 'Meta Keywords: ';
+        $html .= '</label>';
+        $html .=  "<input type='text' id='mtg_keywords_input' name='mtg_keywords_input' value='$keywords'>";
+        $html .= '</div>';
+
+        */
 
         // save button
         $html .=  '<input type="submit" id="mtg_save_button" name="mtg_save_button" value="Save">';
@@ -177,15 +211,15 @@ class MetaTagsGenius
 
 
         // check if saved
-        if ( isset($_GET['updated']) && $_GET['updated'] == 'true') {
+        if (isset($_GET['updated']) && $_GET['updated'] == 'true') {
             $this->mtg_dismissible_message('Settings saved.');
         }
 
         // get meta tags options
-        $meta_tags_options = get_option('mtg_meta_tags_options');
+        $meta_tags = $this->mtg_get_meta_tags();
 
         // show meta tags options page
-        $this->mtg_meta_tags_options_html($meta_tags_options);
+        $this->mtg_meta_tags_options_html($meta_tags);
     }
 }
 
